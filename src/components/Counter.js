0bbb6a46c8df7884d10e868477b701e6250356bd
Justin Benz <jbenz156@gmail.com>
2020-11-02 17:15:44 -0500

    finished spinner and input, working on moods now

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -50,7 +50,7 @@ import React, { useState } from 'react';
 
 export default function Counter() {
   /* STEP 1 */
-  [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
   const increment = () => {
     setCount(count + 1);
   };
--- src/components/Moods.js
+++ src/components/Moods.js
@@ -28,7 +28,7 @@ STEPS 4, 5, 6:
   Inside these click handlers set the correct mood, using 'setMood' and the variables below the imports.
 */
 
-import React from 'react'; /* STEP 0 */
+import React,{ useState } from 'react'; /* STEP 0 */
 
 const initialMood = 'Not sure how I feel';
 const happyMood = 'Quite happy!';
@@ -36,9 +36,10 @@ const sadMood = 'Rather sad';
 
 export default function Moods() {
   /* STEP 1 */
+const[mood, setMood] = useState(initialMood)
 
   const makeHappy = () => {
-    /* STEP 4 */
+    
   };
   const makeSad = () => {
     /* STEP 5 */
@@ -50,7 +51,7 @@ export default function Moods() {
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'crimson', /* STEP 2 */
+    color: (useState === happyMood ? 'royalblue' : 'crimson')
   };
 
   return (
